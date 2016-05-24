1f813a36dc8dbc161649af5622f0b697587cf913
Richard Ma <richard.ma.19850509@gmail.com>
2016-05-24 11:25:42 +0800

    输出文件使用csv格式

--- modules/searchEngine.py
+++ modules/searchEngine.py
@@ -118,7 +118,7 @@ class SearchEngine(object):
 
         """
         # 文件名keyword_bing.urls
-        return "%s_%s.urls" % (keyword, self.engineName)
+        return "%s_%s.csv" % (keyword, self.engineName)
 
     def writeToFile(self, all_url, filename):
         """@todo: 将结果保存到文件
